c4e805656c86ef2dfff70838604f37205f44a61e
Jeff15321 <jeff.lu234@gmail.com>
2025-01-07 13:16:58 -0500

    s

--- src/pages/mongo.tsx
+++ src/pages/mongo.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 import client from "../server/db/mongodb";
 import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
 import { useEffect, useState } from "react";
-
+//
 type ConnectionStatus = {
   isConnected: boolean;
 };
